7b160ca4dd0e49d26aa279619191305e6aed7775
Pete Hall <pete.mack.hall@gmail.com>
2022-08-02 13:37:56 -0500

    getting back to where we were

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+
+const WordOfTheDay = "GHOST";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="board">
+      <h1>Wordle</h1>
+      <Row />
+      <Row />
+      <Row />
+      <Row />
+      <Row />
     </div>
   );
 }
+
+const Row = () => {
+
+  const [newWord, setNewWord] = useState('');
+
+  useEffect(() => {
+    console.log(newWord);
+  }, [newWord])
+
+  const handleChange = (character) => {
+    setNewWord(newWord + character);
+  }
+
+  return (
+    <div className='row'>
+      {[...new Array(5)].map((x, i) => {
+        return (
+          <Cell key={i} handleChange={handleChange}/>
+        )
+
+      })}
+    </div>
+  );
+}
+
+const Cell = (props) => {
+
+  const handleCellChange = (e) => {
+    props.handleChange(e.target.value);
+  }
+
+  return (
+    <div>
+      <input onChange={handleCellChange} type="text" maxLength={1} />
+    </div>
+  );
+}
+
+
 
 export default App;
